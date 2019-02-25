e525dff3eba19096ab337e17c4bf489d73f2be0d
biocodify <47731650+biocodify@users.noreply.github.com>
2019-02-25 01:46:00 +0100

    intro.py

--- intro.py
+++ intro.py
@@ -1,9 +1,5 @@
-#Introduction to python programming
-<<<<<<< HEAD
-#To begin with, a python program that output "Hello world"
-=======
-#To begin with, a python program that output Hello world
->>>>>>> added cone.py
+##A python program 
+#To begin with, a python program that output "Hello World" 
 #Demonstrating the input and output statement in python
 
 print("Hello World")
@@ -15,15 +11,7 @@ print("Welcome to the Biodata page")
 
 
 #Inputing...
-<<<<<<< HEAD
-firstname = input("Enter your Firstname:")
-lastname = input("Enter your Lastname:")
-email = input("Enter your Email:")
-address = input("Enter your address:")
-state_of_origin = input("Enter your state_of_origin:")
-country = Input("Enter your country:")
-phone_number = input("Enter your phone_number")
-=======
+
 firstname = input("Enter your firstname:")
 lastname = input("Enter your lastname:")
 email = input("Enter your email:")
@@ -31,22 +19,16 @@ address = input("Enter your address:")
 state_of_origin = input("Enter your state of origin:")
 country = input("enter your country:")
 phone_number = int(input("Enter your phone number"))
->>>>>>> added cone.py
 
 
 #Outputing...
 print("Your firstname is:" , firstname)
 print("Your lastname is:" , lastname)
 print("Your email is:" , email)
-print("Your address is:" , address)
-<<<<<<< HEAD
-print("Your state_of_origin is:" , state of origin)
+print("Your address is:", address) 
+print("Your state of origin is:" , state_of_origin)
 print("Your country is:" , country)
-print("Your phone number is:" , phone_number)
-=======
-print("Your state_of_origin is:" , state_of_origin)
-print("Your country is:" , country)
-print("Your phone_number is:" , phone_number)
->>>>>>> added cone.py
+print("Your phone number is:", phone_number) 
+
 
 #End
